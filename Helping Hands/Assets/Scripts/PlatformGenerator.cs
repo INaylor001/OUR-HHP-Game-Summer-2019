8027f6ad2f6580acbc78c696f56ba9c8d734cdfc
Irvin Naylor <Inaylor003@gmail.com>
2019-06-24 11:39:15 -0400

    Videos 1-6 done

---
+++ Helping Hands/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+
+    public GameObject thePlatform;
+    public Transform genPoint;
+    public float distanceBetween;
+
+    private float platformWidth;
+
+    public float distanceBetweenMin;
+    public float distanceBetweenMax;
+
+    public ObjectPooler theObjectPool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < genPoint.position.x)
+        {
+            distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+
+            //Instantiate(thePlatform, transform.position, transform.rotation);
+            GameObject newPlatform = theObjectPool.GetPooledObject();
+
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true);
+
+
+        }
+    }
+}
---
+++ Helping Hands/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Collider2D myCollider;
+
+    private Rigidbody2D myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        myCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+
+        myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
+        
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (grounded)
+            {
+
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+            }
+        }
+    }
+}
