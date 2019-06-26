9869fe763e0b4b547225abe6ef6181351f7db497
Irvin Naylor <Inaylor003@gmail.com>
2019-06-26 12:59:05 -0400

    Videos 7-11

--- Helping Hands/Assets/Scripts/PlatformGenerator.cs
+++ Helping Hands/Assets/Scripts/PlatformGenerator.cs
@@ -14,12 +14,36 @@ public class PlatformGenerator : MonoBehaviour
     public float distanceBetweenMin;
     public float distanceBetweenMax;
 
-    public ObjectPooler theObjectPool;
+    //public GameObject[] thePlatforms;
+    private int platformSelector;
+    private float[] platformWidths;
+
+    public ObjectPooler[] theObjectPools;
+
+
+    //height
+    private float minHeight;
+    public Transform maxHeightPoint;
+    private float maxHeight;
+    public float maxHeightChange;
+    private float heightChange;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+        //platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+
+        platformWidths = new float[theObjectPools.Length];
+
+        for (int i = 0; i < theObjectPools.Length; i++)
+        {
+            platformWidths[i] = theObjectPools[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
+        }
+
+        minHeight = transform.position.y;
+        maxHeight = maxHeightPoint.position.y;
+
 
     }
 
@@ -30,14 +54,35 @@ public class PlatformGenerator : MonoBehaviour
         {
             distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
 
-            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+            platformSelector = Random.Range(0, theObjectPools.Length);
 
-            //Instantiate(thePlatform, transform.position, transform.rotation);
-            GameObject newPlatform = theObjectPool.GetPooledObject();
+            heightChange = transform.position.y + Random.Range(maxHeightChange, -maxHeightChange);
+
+            if (heightChange > maxHeight)
+            {
+                heightChange = maxHeight;
+            } else if (heightChange < minHeight)
+            {
+                heightChange = minHeight;
+            }
+
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, heightChange, transform.position.z);
+
+            
+
+            //Instantiate(/*thePlatform,*/ thePlatforms[platformSelector], transform.position, transform.rotation);
+
+
+
+
+
+            GameObject newPlatform = theObjectPools[platformSelector].GetPooledObject();
 
             newPlatform.transform.position = transform.position;
             newPlatform.transform.rotation = transform.rotation;
             newPlatform.SetActive(true);
+
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2), transform.position.y, transform.position.z);
 
 
         }
--- Helping Hands/Assets/Scripts/PlayerControl.cs
+++ Helping Hands/Assets/Scripts/PlayerControl.cs
@@ -5,29 +5,68 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public float moveSpeed;
+    private float moveSpeedStore;
+    public float speedMultiplier;
+
+
+    public float speedIncreaseMilestone;
+    public float speedIncreaseMilestoneStore;
+
+    private float speedMilestoneCount;
+    private float speedMilestoneCountStore;
+
+
+
     public float jumpForce;
+
+    public float jumpTime;
+    private float jumpTimeCounter;
 
     public bool grounded;
     public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float groundCheckRadius;
 
-    private Collider2D myCollider;
+    //private Collider2D myCollider;
 
     private Rigidbody2D myRigidBody;
+
+    public GameManager theGameManager; 
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
 
-        myCollider = GetComponent<Collider2D>();
+        //myCollider = GetComponent<Collider2D>();
+
+        jumpTimeCounter = jumpTime;
+
+        speedMilestoneCount = speedIncreaseMilestone;
+
+        moveSpeedStore = moveSpeed;
+        speedMilestoneCountStore = speedMilestoneCount;
+        speedIncreaseMilestoneStore = speedIncreaseMilestone;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+        //grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
 
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+
+
+        if(transform.position.x > speedMilestoneCount)
+        {
+            speedMilestoneCount += speedIncreaseMilestone;
+
+            speedIncreaseMilestone = speedIncreaseMilestone * speedMultiplier;
+
+            moveSpeed = moveSpeed * speedMultiplier;
+        }
 
         myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
         
@@ -39,5 +78,36 @@ public class PlayerControl : MonoBehaviour
                 myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
             }
         }
+
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (jumpTimeCounter > 0)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKeyUp (KeyCode.Space) || Input.GetMouseButtonUp(0))
+        {
+            jumpTimeCounter = 0;
+        }
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
     }
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.gameObject.tag == "killbox")
+        {
+            theGameManager.RestartGame();
+            moveSpeed = moveSpeedStore;
+            speedMilestoneCount = speedMilestoneCountStore;
+            speedIncreaseMilestone = speedIncreaseMilestoneStore;
+        }
+    }
+
+
 }
